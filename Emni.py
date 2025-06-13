05ff66bb2cd3a53d8f0e5cd2129549e6e06ba6f0
simanta-code <simanta270099@gmail.com>
2025-06-13 18:13:50 +0600

    test

--- Emni.py
+++ Emni.py
@@ -1,13 +1,23 @@
-x = 5
+# x = 5
 
-if x >= 5:
-    print('hello')
+# if x >= 5:
+#     print('hello')
 
-elif x<=5:
-    print('Bye')
+# elif x<=5:
+#     print('Bye')
 
-elif x==5:
-    print('Hi')
+# elif x==5:
+#     print('Hi')
 
-else:
-    print('Nothing ')
+# else:
+#     print('Nothing ')
+
+class classname:
+    def __init__(self, model, price):
+        print(model, price)
+
+v = classname("S24", 150000)
+print(v)
+
+    
+    
